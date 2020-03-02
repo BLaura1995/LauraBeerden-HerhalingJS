c1bfb6c2bad3d0935e8ac33a89198970c6d9de57
Laura Beerden <60176579+BLaura1995@users.noreply.github.com>
2020-03-02 15:36:29 +0100

    aangepast

--- 03-wk2018.js
+++ 03-wk2018.js
@@ -1,6 +1,8 @@
-function toonWinnaars() {
-    var landen = new Array('Frankrijk', "Kroatië", "België");
-    document.write('<p> Op plaats 1 staat : ' + landen[0] + "</p>");
-    document.write('<p> Op plaats 1 staat : ' + landen[1] + "</p>");
-    document.write('<p> Op plaats 1 staat : ' + landen[2] + "</p>");
-}
+  function toonWinnaars() {
+      var winnaars = ["Frankrijk", "Kroatië", "België"];
+      var lengte = winnaars.length;
+      for (var i = 0; i < lengte; i++) {
+          document.write("<p>Op plaats " + (i + 1) + " staat : " + winnaars[i] + ".</p>")
+      }
+  }
+  toonWinnaars();
