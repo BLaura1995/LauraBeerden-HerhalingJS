280eaa5f156c0e191515b170191c7d1db924bc79
Laura Beerden <60176579+BLaura1995@users.noreply.github.com>
2020-03-02 11:18:36 +0100

    03-wk2018

---
+++ 03-wk2018.js
@@ -0,0 +1,6 @@
+function toonWinnaars() {
+    var landen = new Array('Frankrijk', "Kroatië", "België");
+    document.write('<p> Op plaats 1 staat : ' + landen[0] + "</p>");
+    document.write('<p> Op plaats 1 staat : ' + landen[1] + "</p>");
+    document.write('<p> Op plaats 1 staat : ' + landen[2] + "</p>");
+}
